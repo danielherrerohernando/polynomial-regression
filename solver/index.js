91bc4b9d9487c72b828de033e1ed9acc1954e9bb
Daniel Herrero <danielherrerohernando@gmail.com>
2020-05-17 23:49:52 +0200

    feat: adding gauss-jordan solver for matrix equation

---
+++ solver/index.js
@@ -0,0 +1,45 @@
+const deepClone = arr => arr.map(row=>[].concat(row));
+
+const triangularize = (matrix, indep) => {
+  const m = deepClone(matrix);
+  const b = deepClone(indep);
+  const n = m.length;
+  for (let i=0; i<n-1; i++) {
+    for (let j=i+1; j<n; j++) {
+      const c = m[j][i]/m[i][i];
+      for (let k=i+1; k<n; k++) {
+        m[j][k] = m[j][k] - c*m[i][k];
+      }
+      b[j][0] = b[j][0] - c*b[i][0];
+    }
+  }
+  return [m,b];
+};
+
+const backSubstitute = (matrix, indep) => {
+  const n = matrix.length;
+  const x = [];
+  for (let i=n-1; i>=0; i--) {
+    const minus = x.reduce((acc,val,idx)=>acc+=val*matrix[i][n-1-idx],0);
+    const sol = (indep[i][0] - minus)/matrix[i][i];
+    x.push(sol);
+  }
+  return x.reverse();
+};
+
+const solve = (coefficients, independent) => backSubstitute(...triangularize(coefficients, independent));
+
+
+const example = [
+  [1,1,-2],
+  [2,-1,4],
+  [2,-1,6]
+];
+const ind = [
+  [9],
+  [4],
+  [-1]
+];
+
+
+console.log(solve(example, ind));
