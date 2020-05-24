71d0ac80dd4763aa08b54570b560cba5a68c5784
Daniel Herrero <danielherrerohernando@gmail.com>
2020-05-24 23:10:11 +0200

    feat: adding api methods load-save

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,7 +1,12 @@
 const buildArray = size => new Array(size).fill('');
 const deepClone = arr => arr.map(row=>[].concat(row));
 
+const addOperator = s => s[0]==='-' ? s : '+'+s;
+const getExpression = params => params.reduce((acc,el,i)=> acc + addOperator(el.toString().replace('e','E'))+'*x^'+`${i}`,'');
+
+
 module.exports = {
   buildArray,
-  deepClone
+  deepClone,
+  getExpression
 };
--- src/index.js
+++ src/index.js
@@ -1,22 +1,48 @@
+const { writeFileSync, readFileSync } = require('fs');
 const { buildSystem } = require('./systemBuilder');
 const { solve } = require('./systemSolver');
-
+const { getExpression } = require('./helpers');
 
 const createModel = () => {
 
-  const estimator = {};
+  let params = {};
 
-  const fit = data => degree => {
-    const system = buildSystem(data, degree);
-    const coefficients = solve(...system);
-    estimator[degree] = x => coefficients.reduce((acc,c,i)=>acc+c*x**i,0);
+  const fit = (data,degrees) => {
+    degrees.forEach(degree => {
+      const system = buildSystem(data, degree);
+      const coefficients = solve(...system);
+      params[degree] = coefficients;
+    });
   };
 
-  const estimate = degree => x => estimator[degree] && estimator[degree](x);
+  const estimate = (degree,x) => params[degree] && params[degree].reduce((acc,c,i)=>acc+c*x**i,0);
 
-  return {fit, estimate};
+  const loadParams = path => {
+    const loadedData = JSON.parse(readFileSync(path));
+    params = { ...params, ...loadedData };
+  };
+
+  const saveParams = path => writeFileSync(path, JSON.stringify(params, null, 2));
+
+  const saveExpressions = path => {
+    const paramsWithExpressions = Object.entries(params).reduce((acc,[degree,coefficients]) => {
+      acc[degree] = getExpression(coefficients);
+      return acc;
+    },{});
+    writeFileSync(path, JSON.stringify(paramsWithExpressions, null, 2));
+  };
+
+  return { fit, estimate, loadParams, saveParams, saveExpressions };
 };
 
 module.exports = {
   createModel
 };
+
+const {mockData02} = require('../test/mockData');
+
+const model = createModel();
+
+model.fit(mockData02, new Array(28).fill('').map((_,i)=>i));
+
+model.saveExpressions('./saved.json');
